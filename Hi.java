b04a371d1fc863e03c43672ddbe96563d2095942
Odedra Viraj <141026170+virajodedra@users.noreply.github.com>
2023-12-29 10:47:10 +0530

    Add files via upload

---
+++ Hi.java
@@ -0,0 +1,10 @@
+public class Hi{
+
+     public static void main(String []args){
+
+        int a=10;
+        System.out.println("NUMBER IS : " +a);
+
+        
+     }
+}
---
+++ Sum1.java
@@ -0,0 +1,9 @@
+public class Sum1 {
+
+    public static void main(String[] args) {
+        int a=5,b=90;
+
+        System.out.println(a+b);
+    }
+    
+}
