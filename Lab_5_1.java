2653cb454758d4c2ca59eabf55bc06ec1207fe1d
Odedra Viraj <141026170+virajodedra@users.noreply.github.com>
2024-02-15 16:10:28 +0530

    Update Lab_5_1.java

--- Lab_5_1.java
+++ Lab_5_1.java
@@ -1,16 +1,35 @@
 import java.util.*;
+import java.math.*;
 public class Lab_5_1 {
     public static void main(String[] args) {
+        Circle S1 = new Circle();
+        
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Radius :");
-        int r = sc.nextInt();
+        double r = sc.nextDouble();
+
+        double area = S1.Area( r);
+        S1.Print( area);
         
     }
     
 }
 
-  class A{
-    
+  class Circle{
+
+        public double Area(Double r){
+
+        double a = Math.PI*r*r;
+
+        return a;
+        
+
+        }
+
+        public void Print(double a){
+            System.out.println(a);
+
+        }
 
   }
