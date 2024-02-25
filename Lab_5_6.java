e4465335b639fae4f138e1954dd1be4c891754b4
Odedra Viraj <141026170+virajodedra@users.noreply.github.com>
2024-02-25 12:07:16 +0530

    Add files via upload

---
+++ Lab_5_6.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+
+class Count{
+
+    static  int countObject = 0;
+
+    public Count(){
+
+        countObject  += 1;
+
+    }
+
+    public void printCount(){
+        System.out.println("the number of of  objects are : "+countObject);
+
+    }
+     
+}
+
+public class Lab_5_6 {
+    
+    
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+     Count c1 = new Count();
+     Count c2 = new Count();
+     Count c3 = new Count();
+     Count c4 = new Count();
+     Count c5 = new Count();
+     Count c6 = new Count();
+     Count c7 = new Count();
+     Count c8 = new Count();
+     Count c9 = new Count();
+     Count c10 = new Count();
+     Count c11 = new Count();
+     Count c12 = new Count();
+     Count c13 = new Count();
+     Count c14 = new Count();
+        
+     
+     c1.printCount();
+     
+
+     
+
+    }
+    
+}
