8368fc7106e6a604f99e533b8f66c7d1fd9d9103
Odedra Viraj <141026170+virajodedra@users.noreply.github.com>
2024-03-16 11:09:53 +0530

    Create Lab_7_1.java

---
+++ LAB7/Lab_7_1.java
@@ -0,0 +1,60 @@
+abstract class Vegetable{
+    String color;
+    Vegetable(String color){
+        this.color = color;
+    
+    }
+
+    abstract public String toString();
+}
+
+class Potato extends Vegetable{
+
+      Potato(String color){
+        super(color);
+
+      }
+    public String toString(){
+        
+     return "Vegetale :potato " +color; 
+ 
+    }
+} 
+class Brinjal extends Vegetable{
+
+    Brinjal(String color){
+        super(color);
+    }
+  public String toString(){
+      
+   return "Vegetale :Brinjal " +color; 
+
+  
+  }
+} 
+class  Tomato extends Vegetable{
+
+    Tomato(String color){
+        super(color);
+    }
+  public String toString(){
+      
+   return "Vegetale :Tomato " +color; 
+
+  
+  }
+} 
+
+public class Lab_7_1 {
+    public static void main(String[] args) {
+        Potato p =new Potato("skinny");
+        System.out.println(p);
+        Brinjal b =new Brinjal("violet");
+        System.out.println(b);
+        Tomato t =new Tomato("red");
+        System.out.println(t);
+
+
+    }
+    
+}
