7215a9960cc2c9d8ec01217167921784dc35827a
Odedra Viraj <141026170+virajodedra@users.noreply.github.com>
2024-03-29 11:29:36 +0530

    Create Lab_9_1.java

---
+++ LAB 9/Lab_9_1.java
@@ -0,0 +1,51 @@
+class MyThread extends Thread{
+    
+     public void run(){
+        for(int i=0;i<20;i++){
+
+             System.out.println("Good Morning");
+             try{
+                Thread.sleep(1000);
+             }catch(Exception e){
+
+                e.printStackTrace();
+             }
+            
+        }
+        
+    }
+}
+
+class MyThread2 extends Thread{
+    
+    public void run(){
+        for(int i=0;i<20;i++){
+
+            System.out.println("Good Afternon");
+            try{
+               Thread.sleep(3000);
+            }catch(Exception e){
+        
+               e.printStackTrace();
+            }
+           
+        }
+       
+   }
+}
+
+
+
+
+
+
+public class Lab_9_1{
+    public static void main(String[] args) {
+        MyThread myt1 = new MyThread();
+        MyThread2 myt2 = new MyThread2();
+
+        myt1.start();
+        myt2.start();
+        
+    }
+}
