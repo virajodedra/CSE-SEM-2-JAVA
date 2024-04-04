aa374e232bfb17dc9da77e53cdf29b082d5361fd
Odedra Viraj <141026170+virajodedra@users.noreply.github.com>
2024-04-04 22:23:42 +0530

    Create Input_Arg.java

---
+++ LAB 1/Input_Arg.java
@@ -0,0 +1,8 @@
+public class Input_Arg {
+    public static void main(String[] args) {
+      String p = args[0];
+      int a = Integer.parseInt(args[1]);
+      System.out.println(p + " " + a);
+        
+    }
+}
