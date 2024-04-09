d89591d033e9f7130928667bc3f0f0330f324054
Odedra Viraj <141026170+virajodedra@users.noreply.github.com>
2024-04-09 21:27:52 +0530

    Update Lab_8_2.java

--- LAB8/Lab_8_2.java
+++ LAB8/Lab_8_2.java
@@ -1,30 +1,32 @@
-public class Lab_8_2 {
+
+public class Lab8_2 {
     public static void main(String[] args) {
+        int arrLength = args.length;
+        int[] arr = new int[arrLength];
+        int sum = 0;
         try{
-            int a = Integer.parseInt(args[0]);
-            int[] arr = new int[a];
+            for(int i=0;i<arrLength;i++){
+                arr[i] = Integer.parseInt(args[i]);
 
-            try{
-                for(int i=0;i<a;i++){
-                    arr[i] = Integer.parseInt(args[i+1]);
-                     try{
-                        if(arr[i]<0){
-                            throw new Exception(arr[i]+ " must be positive");
-                        }
-                     }
+                if(arr[i]<0){
+                    throw new Exception("Number is negative : "+arr[i]);
                 }
-
-                
-
+                if(arr[i]%10 == 0){
+                    throw new Exception("Number is divisible by 10 : "+arr[i]);
+                }
+                if(arr[i]>1000 && arr[i]<2000){
+                    throw new Exception("Number is between 1000 and 2000 : "+arr[i]);
+                }
+                if(arr[i]>7000){
+                    throw new Exception("Number is greater than 7000 : "+arr[i]);
+                }
+                sum+=arr[i];
             }
-        
-        }catch(Exception e){
-            System.out.println(e.getMessage());
+        }
+        catch(Exception e){
+            e.printStackTrace();
         }
 
-
-
-
-    }
-    
+        System.out.println("Sum : "+sum);
+    }   
 }
