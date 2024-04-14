f7edfc120e4db675e3f12f4aeb8f228cc9ad25ae
Odedra Viraj <141026170+virajodedra@users.noreply.github.com>
2024-04-14 13:06:21 +0530

    Create  Lab_12_StudentByMe.java
    made by me easy to understand...

---
+++ LAB 12/Lab_12_StudentByMe.java
@@ -0,0 +1,47 @@
+
+import java.util.*;
+// import java.io.*;
+ class Student{
+    String name;
+    int age;
+    Student(String name , int age){
+        this.name = name;
+        this.age = age;
+    }
+ }
+
+ class AgeComparator implements Comparator<Student>{
+    public int compare(Student s1,Student s2) {
+        
+
+        if(s1.age==s2.age)
+            return 0;
+        
+        else if(s1.age>s2.age)
+            return 1;
+        
+         else
+            return -1;
+        
+    }
+ }
+
+public class Lab_12_StudentByMe {
+    public static void main(String[] args) {
+        ArrayList<Student> al = new ArrayList<Student>();
+    al.add(new Student("Viraj",18));
+    al.add(new Student("Manvendra",19));
+    al.add(new Student("Pruthviraj",17));
+
+     System.out.println("Soring by age");
+     Collections.sort(al,new AgeComparator());
+     Iterator<Student> it = al.iterator();
+
+     while (it.hasNext()) {
+        Student st = it.next();
+        System.out.println(st.name + " " + st.age);
+        
+     }
+     
+    }
+}
